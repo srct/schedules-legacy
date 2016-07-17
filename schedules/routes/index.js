c473aa7a5d1799329cb882707e6d2f0deb70681c
mark@stengle.in <mstengle@gmu.edu>
2016-07-17 17:59:32 -0400

    Added bootstrap
    - Sorry :dhaynes: it's not masonstrap, but for good reason
    - This is set up as sass, and it autocompiles when you change the underlying sass

--- schedules/routes/index.js
+++ schedules/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/docs', function(req, res, next) {
+  res.render('docs', { title: 'Schedules' });
+})
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Schedules' });
 });
 
 module.exports = router;
