d7ba538b35a837bad5b175a9a3959a14b83d5c75
Zach Knox <zmknox@gmail.com>
2016-07-17 11:42:01 -0400

    Added support for LOTS of package managers

--- configure.sh
+++ configure.sh
@@ -4,14 +4,20 @@ haveProg() {
 }
 
 if haveProg apt-get ; then func_apt-get
-elif haveProg yum ; then func_yum #TODO
-elif haveProg yum ; then func_packman #TODO
+elif haveProg yum ; then func_yum
+elif haveProg pacman ; then func_pacman
 elif haveProg brew ; then func_brew
+elif haveProg emerge ; then func_emerge
+elif haveProg zypper ; then func_zypper
+elif haveProg pkg ; then func_pkg
+elif haveProg port ; then func_port
+
 else
-    echo 'No package manager found!'
+    echo 'No supported package manager found!'
     exit 2
 fi
 
+#TODO: Add 'update' commands to these commands
 func_apt-get() {
     if
     # Install nodejs
@@ -20,14 +26,28 @@ func_apt-get() {
     sudo apt-get install nodejs
 }
 func_yum() {
-    #TODO
+    sudo curl --silent --location https://rpm.nodesource.com/setup_4.x | bash -
+    sudo yum -y install nodejs
 }
-func_packman() {
-    #TODO
+func_pacman() {
+    sudo pacman -S nodejs npm
 }
 func_brew() {
     brew update
     brew install node npm #both, just in case
+}
+func_zypper() {
+    echo "Go here: http://software.opensuse.org/download.html?project=devel%3Alanguages%3Anodejs&package=nodejs"
+    #Because the SuSE commands were version specific so I give up
+}
+func_emerge() {
+    emerge nodejs
+}
+func_pkg() {
+    pkg install node
+}
+func_port() {
+    port install nodejs
 }
 
 # Install npm dependencies
