ca4b6ef69701295a3a04708da59e90d9a889019c
Mark Stenglein <mark@gmu.edu>
2016-07-17 01:06:57 -0400

    Fix bug in the setup script

--- configure.sh
+++ configure.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+# Install nodejs
 curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
 sudo apt-get update
 sudo apt-get install nodejs
+# Install npm dependencies
 npm install
 cd schedules
 npm install
 cd ..
-./start
+# Start the server
+./start.sh
