443077e4eb4e9685b7b51b805a438c8120672dc1
Zach Knox <zmknox@gmail.com>
2016-07-17 11:26:39 -0400

    package manager detection and Homebrew commands

--- configure.sh
+++ configure.sh
@@ -1,8 +1,35 @@
 #!/bin/bash
-# Install nodejs
-curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
-sudo apt-get update
-sudo apt-get install nodejs
+haveProg() {
+    [ -x "$(which $1)" ]
+}
+
+if haveProg apt-get ; then func_apt-get
+elif haveProg yum ; then func_yum #TODO
+elif haveProg yum ; then func_packman #TODO
+elif haveProg brew ; then func_brew
+else
+    echo 'No package manager found!'
+    exit 2
+fi
+
+func_apt-get() {
+    if
+    # Install nodejs
+    curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+    sudo apt-get update
+    sudo apt-get install nodejs
+}
+func_yum() {
+    #TODO
+}
+func_packman() {
+    #TODO
+}
+func_brew() {
+    brew update
+    brew install node npm #both, just in case
+}
+
 # Install npm dependencies
 npm install
 cd schedules
