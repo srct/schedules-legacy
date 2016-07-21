e97eeee55fdaa3ef67b01a59c7f9bf79364841f4
mark@stengle.in <mstengle@gmu.edu>
2016-07-21 19:10:26 -0400

    Finish adding server-side save functionality
    - should _definitely_ add some checks to make sure user's don't give bad input

--- schedules/routes/index.js
+++ schedules/routes/index.js
@@ -11,18 +11,28 @@ router.get('/docs', function(req, res, next) {
 // TODO: Actually learn how to make this function set be passed around properly
 
 /* GET home page. */
+// Save URL for entire schedules
 router.get('/:school/:semester/:crns', function(req, res, next) {
   var crns = req.params['crns'].split(',');
+  var semester = req.params['semester'];
+  var school = req.params['school'];
+  res.render('index', { config, crns, semester, school });
 })
 
+// Save URL for everything up to semester
 router.get('/:school/:semester', function(req, res, next) {
   var semester = req.params['semester'];
+  var school = req.params['school'];
+  res.render('index', { config, semester, school });
 })
 
+// Save URL for school
 router.get('/:school', function(req, res, next) {
   var school = req.params['school'];
+  res.render('index', { config, school });
 })
 
+// All defaults
 router.get('/', function(req, res, next) {
   res.render('index', { config });
 })
