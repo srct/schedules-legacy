79a389f422435f1afad6b4beed5d03f706eb44dd
mark@stengle.in <mstengle@gmu.edu>
2016-07-21 18:09:27 -0400

    disable broken code on git

--- schedules/routes/index.js
+++ schedules/routes/index.js
@@ -11,23 +11,23 @@ router.get('/docs', function(req, res, next) {
 // TODO: Actually learn how to make this function set be passed around properly
 
 /* GET home page. */
-router.get('/:school/:semester/:crns'), function(req, res, next) {
-  var crns = req.params['crns'].split(',');
-}
-
-router.get('/:school/:semester'), function(req, res, next) {
-  var semester = req.params['semester'];
-  next();
-}
-
-router.get('/:school'), function(req, res, next) {
-  var school = req.params['school'];
-  next();
-}
+// router.get('/:school/:semester/:crns'), function(req, res, next) {
+//   var crns = req.params['crns'].split(',');
+// }
+//
+// router.get('/:school/:semester'), function(req, res, next) {
+//   var semester = req.params['semester'];
+//   next();
+// }
+//
+// router.get('/:school'), function(req, res, next) {
+//   var school = req.params['school'];
+//   next();
+// }
 
 router.get('/'), function(req, res, next) {
   var save = {
-    
+
   }
   render('index', { config });
 }
