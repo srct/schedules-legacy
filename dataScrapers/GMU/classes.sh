4eff95d80130b4a2e6b9057932e97ef70e69638d
mark@stengle.in <mstengle@gmu.edu>
2016-07-23 08:37:39 -0400

    Added sample mongoose model

--- dataScrapers/GMU/classes.sh
+++ dataScrapers/GMU/classes.sh
@@ -1,4 +1,6 @@
 rm classes2016.json
 touch classes2016.json
 node extract2016.js
-code classes2016.json
+
+echo "Copy the json file to the appropriate folder in the app and"
+echo "add it's information to "
