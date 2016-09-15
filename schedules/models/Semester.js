cc8928d30780398109990add669ae0e2219991cb
Mark Stenglein <mstengle@gmu.edu>
2016-09-15 15:14:46 -0400

    move inner schema to separate file

--- schedules/models/Semester.js
+++ schedules/models/Semester.js
@@ -1,32 +1,14 @@
 // grab the things we need
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var sectionSchema = require('./sectionSchema');
 
 // create a schema
 var semesterSchema = new Schema({
   school: { type: String, required: true },
   semester: { type: String, required: true },
   slug: { type: String, required: true },
-  classes: [
-    {
-      crn: { type: String, required: true },
-      name: { type: String, required: true },
-      title: { type: String, required: true },
-      section: { type: String, required: true },
-      campus: { type: String, required: true },
-      session_templates: [
-        {
-          date_range: { type: String, required: true },
-          days: { type: String },
-          time: { type: String },
-          location: { type: String, required: true },
-          class_type: { type: String, required: true },
-          instructors: { type: String, required: true },
-        }
-      ],
-      notification: { type: String, required: true }
-    }
-  ]
+  classes: [ sectionSchema ]
 });
 
 semesterSchema.set('redisCache', true);
