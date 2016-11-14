608856658d8715ecfb3f706c6caa16ea0145f0a0
Mark Stenglein <mark@stengle.in>
2016-11-13 22:51:29 -0500

    extend runtime for the standard linting

--- test/standard_test.js
+++ test/standard_test.js
@@ -1,2 +1,5 @@
-/* global it */
-it('conforms to standard', require('mocha-standard'))
+/* global it describe */
+describe('standardjs linting', function () {
+  this.timeout(20000)
+  it('conforms to standard', require('mocha-standard'))
+})
