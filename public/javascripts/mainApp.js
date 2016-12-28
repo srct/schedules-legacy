db0f60df7fb1476d71434bd9883a9e0d4449a0a2
Mark Stenglein <mark@stengle.in>
2016-12-27 19:40:02 -0500

    Revert "Nuke the entire front end :boom:"
    This reverts commit 17beb85936707d7854f0a94b56b0ee9b61ee8d28.

---
+++ public/javascripts/mainApp.js
@@ -0,0 +1,50 @@
+/* globals $ Option */
+// Expose the school data globally.
+
+// var schoolData = {}
+
+// Expose the semester data globally
+var semesterData = {}
+
+$(document).ready(function () {
+    // Convert table to DataTable
+  $('#classSearch').DataTable()
+
+    // Get schoolData and populate the selection menus
+  $.getJSON('/api/v1/json/schools', function (schoolSlugs) {
+    semesterData = schoolSlugs
+    populateBoxes()
+  })
+
+  $('#selectSchool').change(function () {
+    populateSemesterSelect()
+  })
+})
+
+function populateBoxes () {
+  var schoolSelect = document.getElementById('selectSchool')
+
+  for (var i = 0; i < semesterData.schools.length; i++) {
+    var schoolName = semesterData[semesterData.schools[i]]
+    schoolSelect.add(new Option(schoolName.longName, i))
+  }
+
+  populateSemesterSelect()
+}
+
+function populateSemesterSelect () {
+  // var schoolSelect = $('#selectSchool')
+  var selectSemester = document.getElementById('selectSemester')
+
+  // Clear Select Semester
+  selectSemester.innerHTML = ''
+
+  var schoolID = $('#selectSchool option:selected').val()
+  var schoolSlug = semesterData.schools[schoolID]
+  var school = semesterData[schoolSlug]
+
+  for (var i = 0; i < school.semesters.length; i++) {
+    var semesterName = school.semesters[i]
+    selectSemester.add(new Option(semesterName.longName, i))
+  }
+}
